e62f9cc384178b449eaf84c805b91fb7eea3c94d
Ryan Witts <106010989+Kame-Ry@users.noreply.github.com>
2025-04-01 16:56:25 +0100

    Add files via upload

---
+++ assets/functions/Project-Links.js
@@ -0,0 +1,18 @@
+const postFiles = [
+    { file: "games_link.html", date: "2025-03-27"},
+    { file: "landscape_link.html", date: "2025-03-27"},
+    { file: "exchange_external_tag_link.html", date: "2025-03-27"},
+    { file: "plex_watched_movie_link.html", date: "2025-03-16"},
+    { file: "weather_tracker_link.html", date: "2025-03-15"},
+    { file: "film_affinity_link.html", date: "2025-03-14"},
+    { file: "contact_form_project_link.html", date: "2025-03-14"},
+    { file: "quotes_project_link.html", date: "2025-03-13"},
+    { file: "seaglass_project_link.html", date: "2025-03-10"},
+    { file: "tags_article_link.html", date: "2025-03-10"},
+];
+
+postFiles.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+console.log("Final sorted postFiles:", postFiles);
+
+export default postFiles;
